0202f8134e1ca9b23b6fffb738aa7740b421f728
Sardor.Mutalov <45397981+SardorMutalov@users.noreply.github.com>
2019-04-14 15:57:39 +0300

    первая удачная сборка; Альфа

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,26 @@
-var gulp = require('gulp'),
-    minCss = require('gulp-clean-css'),
-    sass = require('gulp-sass'),
+var gulp        = require('gulp'),
     browserSync = require('browser-sync').create(),
-    autoprefixer  = require('gulp-autoprefixer');;
+    sass        = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
-    // Static Server + watching scss/html files
+// Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "src/"
+        server: "./src"
     });
+
+    gulp.watch("src/sass/*.scss", ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-sass.compiler = require('node-sass');
-
 gulp.task('sass', function() {
-  return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('./src/css'))
-    .pipe(browserSync.reload({stream: true}));
-});
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+    return gulp.src("src/sass/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
